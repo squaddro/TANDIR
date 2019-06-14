d1d10489d4c0829bc5c9ea42a95750af816dacef
BatuhanUlucay <hulucay@etu.edu.tr>
2019-06-15 00:54:34 +0300

    added deletion and update for recipes

--- app/src/main/java/com/squadro/tandir/RecipePageActivity.java
+++ app/src/main/java/com/squadro/tandir/RecipePageActivity.java
@@ -1,14 +1,18 @@
 package com.squadro.tandir;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -159,7 +163,7 @@ public class RecipePageActivity extends AppCompatActivity {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position,
+            public void onItemClick(AdapterView<?> parent, View view, final int position,
                                     long id) {
 
                 AlertDialog.Builder desc =
@@ -168,9 +172,161 @@ public class RecipePageActivity extends AppCompatActivity {
                 desc.setMessage(recipes[position].getRecipe_desc())
                         .setCancelable(true);
 
+                desc.setNeutralButton("Delete",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+
+                                    final String recipeId = recipes[position].getRecipe_id();
+                                    deleteRecipe(recipeId);
+                                    finish();
+                                    startActivity(getIntent());
+
+                                dialog.cancel();
+                            }
+                        });
+
+                desc.setNegativeButton("Update1",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ///updateee
+                                recipe_id = recipes[position].getRecipe_id();
+                                recipe_name = recipes[position].getRecipe_name();
+                                recipe_desc = recipes[position].getRecipe_desc();
+
+
+                                AlertDialog.Builder updateAlert =
+                                        new AlertDialog.Builder(RecipePageActivity.this);
+
+
+
+                                final EditText inputName = new EditText(RecipePageActivity.this);
+                                final EditText inputDesc = new EditText(RecipePageActivity.this);
+
+
+                                inputName.setText(recipe_name);
+                                inputDesc.setText(recipe_desc);
+
+                                Context context = updateAlert.getContext();
+                                LinearLayout layout = new LinearLayout(context);
+
+                                layout.setOrientation(LinearLayout.VERTICAL);
+
+
+
+                                inputName.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+                                layout.addView(inputName);
+
+                                inputDesc.setInputType(InputType.TYPE_CLASS_TEXT |InputType.TYPE_TEXT_VARIATION_NORMAL);
+                                layout.addView(inputDesc);
+
+                                updateAlert.setView(layout);
+
+                                updateAlert.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                        String updatedName = inputName.getText().toString();
+                                        String updatedDesc = inputDesc.getText().toString();
+
+                                        if(updatedName.equals(recipe_name)){
+                                            updatedName = null;
+                                        }
+                                        if(updatedDesc.equals(recipe_desc)){
+                                            updatedDesc = null;
+                                        }
+
+                                        updateRecipe(recipes[position].getRecipe_id(), updatedName, updatedDesc);
+                                        finish();
+                                        startActivity(getIntent());
+
+                                    }
+                                });
+
+                                updateAlert.create().show();
+                              //  ((Dialog)dialog).getWindow().setLayout(600,800);
+                                //dialog.cancel();
+                            }
+                        });
                 desc.create().show();
             }
         });
+    }
+
+    private void updateRecipe(String recipe_id, String updatedName, String updatedDesc) {
+
+        rc = new RetrofitCreate();
+        retrofit = rc.createRetrofit();
+
+        rest = retrofit.create(RestAPI.class);
+        JsonObject obj = new JsonObject();
+        obj.addProperty("recipe_name",updatedName);
+        obj.addProperty("recipe_desc",updatedDesc);
+        obj.addProperty("recipe_id",recipe_id);
+
+        Call<JsonObject> call = rest.updateRecipe(obj);
+
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                Status status = new Status(response.body().get("status").getAsInt(),
+                        response.body().get("message").toString());
+
+                Toast.makeText(getBaseContext(),status.toString(),Toast.LENGTH_LONG).show();
+
+                //TODO
+                //if status is blabla
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(getBaseContext(),"Error!",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    private void deleteRecipe(String recipeId){
+
+        recipe_desc = null;
+        recipe_name = null;
+        user_id = null;
+
+        rc = new RetrofitCreate();
+        retrofit = rc.createRetrofit();
+
+        rest = retrofit.create(RestAPI.class);
+        JsonObject obj = new JsonObject();
+        obj.addProperty("recipe_name",recipe_name);
+        obj.addProperty("recipe_desc",recipe_desc);
+        obj.addProperty("user_id",user_id);
+        obj.addProperty("recipe_id",recipeId);
+
+        Call<JsonObject> call = rest.deleteRecipe(obj);
+
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                Status status = new Status(response.body().get("status").getAsInt(),
+                        response.body().get("message").toString());
+
+                Toast.makeText(getBaseContext(),status.toString(),Toast.LENGTH_LONG).show();
+
+                //TODO
+                //if status is blabla
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(getBaseContext(),"Error!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
 
 }
